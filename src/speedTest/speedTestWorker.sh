304202c391a98369603f6347001d04bf9988f5eb
Beeno Tung <aabbcc1241@yahoo.com.hk>
2015-03-06 23:16:00 +0800

    normal save

--- src/speedTest/speedTestWorker.sh
+++ src/speedTest/speedTestWorker.sh
@@ -17,9 +17,28 @@ chmod +x speedtest_cli.py
 # loop with timer #
 
 # start test #
+echo ''
 echo '' >> log
-date >> log
+echo ''
+echo '' >> log
 # sydney
+echo ''
+echo '' >> log
+date
+date >> log
+echo sydney
+echo sydney >> log
 ./speedtest_cli.py --share --server $(grep -i sydney server.list | head -n 1 | awk -F ')' '{print $1}') >> log
 # shenzhen
+echo ''
+echo '' >> log
+date
+date >> log
+echo shenzhen
+echo shenzhen >> log
 ./speedtest_cli.py --share --server $(grep -i shenzhen server.list | head -n 1 | awk -F ')' '{print $1}') >> log
+
+
+echo waiting next time
+
+echo end
