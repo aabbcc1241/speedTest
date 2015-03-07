03ccaf243aeeee3dd2fcf04f4e7631b2b0aa92c9
Beeno Tung <aabbcc1241@yahoo.com.hk>
2015-03-07 21:43:25 +0800

    added timeout (20 second)

--- src/speedTest/speedTestWorker.sh
+++ src/speedTest/speedTestWorker.sh
@@ -8,9 +8,6 @@ echo fetching server list...
 ./speedtest_cli.py --list > server.list
 
 # start test #
-echo '' >> log
-echo ''
-echo '' >> log
 
 # Sydney
 echo ''
@@ -19,7 +16,7 @@ date
 date >> log
 echo ==Sydney==
 echo ==Sydney== >> log
-./speedtest_cli.py --share --server $(grep -i Sydney server.list | head -n 1 | awk -F ')' '{print $1}') >> log
+./speedtest_cli.py --share --timeout=20 --server $(grep -i Sydney server.list | head -n 1 | awk -F ')' '{print $1}') >> log
 
 # Shenzhen
 echo ''
@@ -28,7 +25,7 @@ date
 date >> log
 echo ==Shenzhen==
 echo ==Shenzhen== >> log
-./speedtest_cli.py --share --server $(grep -i Shenzhen server.list | head -n 1 | awk -F ')' '{print $1}') >> log
+./speedtest_cli.py --share --timeout=20 --server $(grep -i Shenzhen server.list | head -n 1 | awk -F ')' '{print $1}') >> log
 
 # Hong Kong
 echo ''
@@ -37,7 +34,7 @@ date
 date >> log
 echo ==Hong Kong==
 echo ==Hong Kong== >> log
-./speedtest_cli.py --share --server $(grep -i 'Hong Kong' server.list | head -n 1 | awk -F ')' '{print $1}') >> log
+./speedtest_cli.py --share --timeout=20 --server $(grep -i 'Hong Kong' server.list | head -n 1 | awk -F ')' '{print $1}') >> log
 
 # Taipei
 echo ''
@@ -46,7 +43,7 @@ date
 date >> log
 echo ==Taipei==
 echo ==Taipei== >> log
-./speedtest_cli.py --share --server $(grep -i Taipei server.list | head -n 1 | awk -F ')' '{print $1}') >> log
+./speedtest_cli.py --share --timeout=20 --server $(grep -i Taipei server.list | head -n 1 | awk -F ')' '{print $1}') >> log
 
 # United Kingdom
 echo ''
@@ -55,7 +52,7 @@ date
 date >> log
 echo ==United Kingdom==
 echo ==United Kingdom== >> log
-./speedtest_cli.py --share --server $(grep -i 'United Kingdom' server.list | head -n 1 | awk -F ')' '{print $1}') >> log
+./speedtest_cli.py --share --timeout=20 --server $(grep -i 'United Kingdom' server.list | head -n 1 | awk -F ')' '{print $1}') >> log
 
 # Canada
 echo ''
@@ -64,7 +61,7 @@ date
 date >> log
 echo ==Canada==
 echo ==Canada== >> log
-./speedtest_cli.py --share --server $(grep -i Canada server.list | head -n 1 | awk -F ')' '{print $1}') >> log
+./speedtest_cli.py --share --timeout=20 --server $(grep -i Canada server.list | head -n 1 | awk -F ')' '{print $1}') >> log
 
 # New York
 echo ''
@@ -73,4 +70,4 @@ date
 date >> log
 echo ==New York==
 echo ==New York== >> log
-./speedtest_cli.py --share --server $(grep -i 'New York' server.list | head -n 1 | awk -F ')' '{print $1}') >> log
+./speedtest_cli.py --share --timeout=20 --server $(grep -i 'New York' server.list | head -n 1 | awk -F ')' '{print $1}') >> log
