f2ed718f065f857edd14701ddf7d0af3b37f231e
Beeno Tung <aabbcc1241@yahoo.com.hk>
2015-03-07 09:37:53 +0800

    ok to release (launch)

--- src/speedTest/setup.sh
+++ src/speedTest/setup.sh
@@ -2,12 +2,10 @@
 # make sure python is installed
 #sudo apt-get update -y
 #sudo apt-get install -y python
-#sudo apt-get install -y python2
-#sudo apt-get install -y python3
-#sudo apt-get install -y wget
 
 # prepare speedtest_cli.py
 #wget -O speedtest-cli.py https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest_cli.py
 
-chmod +x *
+chmod +x *.sh
+chmod +x *.py
 ./speedTestTimer.sh
